3a4b427449ec9fbbd3408170808d2691e0c8b095
Jaideep Nandy <jainandy89@gmail.com>
2018-09-03 17:33:05 +0530

    Updated NetworkConfig.sh

--- config/org1/NewNetwrkconfig.sh
+++ config/org1/NewNetwrkconfig.sh
@@ -1,6 +1,6 @@
 
-#file=$1
-export file=Org1_api_profile.json
+file=$1
+#export file=Org1_api_profile.json
 export API_HOST=$(jq -r '.url' $file)
 export NETWORK_ID=$(jq -r '.network_id'  $file)
 export key=$(jq -r '.key'  $file)
@@ -13,6 +13,25 @@ echo "URL: $API_HOST/api/v1/networks/$NETWORK_ID/connection_profile"
 echo "Authentication: Basic $AUTH"
 
 #curl -X GET "$API_HOST/api/v1/networks/$NETWORK_ID/connection_profile" -H  "accept: application/json" -H  "authorization: Basic $AUTH" > ConnPrfl.json
+
+echo "Getting the Instantiated chaincodes"
+echo "URL:  $API_HOST/api/v1/networks/$NETWORK_ID/channels/defaultchannel/chaincode/instantiated"
+
+
+INST_RESP=$(curl -X POST \
+  "$API_HOST/api/v1/networks/$NETWORK_ID/channels/defaultchannel/chaincode/instantiated" \
+  -H  "Content-Type: application/json" \
+  -H "accept: application/json" \
+  -H  "authorization: Basic $AUTH" \
+    -d "{  \"peer_names\": [    \"org1-peer1\"  ],  \"SKIP_CACHE\": true}" \
+  --connect-timeout 60 || true)
+
+
+  echo ******
+  echo "Chaincode Instantiated Response"
+  echo $INST_RESP
+
+  echo *******
 
 echo "Install chaincode"
 echo "URL: $API_HOST/api/v1/networks/$NETWORK_ID/chaincode/install"
@@ -43,6 +62,7 @@ echo " Install URL:$API_HOST/api/v1/networks/$NETWORK_ID/chaincode/install "
 #-H  "Content-Type: multipart/form-data" \
 #-F "files=$goRoot.zip;type=application/x-zip-compressed" -F "chaincode_id=$ccid" -F "chaincode_version=$new_cc_version" \
 #-F "chaincode_type=golang")
+
 
 echo ******
 echo "Install Response"
